1cb59609542d4d61c8560f803a942f7950b58df6
mannjaro <39473663+mannjaro@users.noreply.github.com>
2025-08-03 18:57:18 +0900

    feat: add support for react-router-with-query and update router context

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,18 +1,25 @@
 // src/router.tsx
-import { createRouter as createTanStackRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen.js'
+import { QueryClient } from "@tanstack/react-query";
+import { createRouter as createTanStackRouter } from "@tanstack/react-router";
+import { routerWithQueryClient } from "@tanstack/react-router-with-query";
+import { routeTree } from "./routeTree.gen.js";
 
 export function createRouter() {
+  const queryClient = new QueryClient();
+
   const router = createTanStackRouter({
     routeTree,
+    context: {
+      queryClient,
+    },
     scrollRestoration: true,
-  })
+  });
 
-  return router
+  return routerWithQueryClient(router, queryClient);
 }
 
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
-    router: ReturnType<typeof createRouter>
+    router: ReturnType<typeof createRouter>;
   }
 }
