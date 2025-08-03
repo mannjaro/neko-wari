f78c7e10f88e6b90354e9ffc3a4643c3d89e70f0
mannjaro <39473663+mannjaro@users.noreply.github.com>
2025-08-03 22:17:31 +0900

    feat: enhance monthly cost fetching with year and month parameters

--- frontend/src/server/getMonthly.ts
+++ frontend/src/server/getMonthly.ts
@@ -4,19 +4,35 @@ import { hc } from "hono/client";
 import type { AppType } from "../../../lambda/backend/app";
 import { getBindings } from "@/utils/binding";
 
+import { z } from "zod";
+
+const Partition = z.object({
+  year: z.number().min(2025),
+  month: z.number().min(1).max(12),
+});
+
 export const getMonthlyCost = createServerFn({
   method: "GET",
-}).handler(async () => {
-  const env = getBindings();
-  const client = hc<AppType>(env.BACKEND_API);
-  const response = await client.dashboard.monthly.$get({
-    query: {
-      month: "08",
-      year: "2025",
-    },
+})
+  .validator((partition: unknown) => {
+    const data = Partition.parse(partition);
+    return {
+      year: data.year.toString(),
+      month: data.month.toString().padStart(2, "0"),
+    };
+  })
+  .handler(async ({ data: { year, month } }) => {
+    const env = getBindings();
+    const client = hc<AppType>(env.BACKEND_API);
+    console.log(year, month);
+    const response = await client.dashboard.monthly.$get({
+      query: {
+        month,
+        year,
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Fetch failed");
+    }
+    return response.json();
   });
-  if (!response.ok) {
-    throw new Error("Fetch failed");
-  }
-  return response.json();
-});
