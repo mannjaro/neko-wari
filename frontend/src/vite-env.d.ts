c9f08fa18f7833c3796eabb382013813698621de
mannjaro <39473663+mannjaro@users.noreply.github.com>
2026-03-15 10:39:52 +0900

    remove lockfile

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -3,6 +3,7 @@ interface ImportMetaEnv {
   readonly VITE_COGNITO_DOMAIN: string;
   readonly VITE_REDIRECT_URI: string;
   readonly VITE_COGNITO_AUTHORITY_URL: string;
+  readonly VITE_LOCAL_AUTH: string;
 }
 
 interface ImportMeta {
